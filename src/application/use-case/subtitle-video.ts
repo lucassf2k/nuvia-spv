fb63a287e198fe97642259313c088238748137d3
lucas vinicius <lucassilved@gmail.com>
2024-05-31 19:42:04 -0300

    feat: add use case subtitle video

---
+++ src/application/use-case/subtitle-video.ts
@@ -0,0 +1,15 @@
+import type { SubtitleVideoDTO } from '../../infrastructure/dtos/subtitle-video-dto'
+
+export type SubtitleVideoInput = {
+	file: string
+}
+
+export type SubtitleVideoOutput = {
+	error: boolean
+	message: string
+	data: unknown
+}
+
+export function subtitleVideo(input: SubtitleVideoInput): SubtitleVideoOutput {
+	return { error: false, message: 'erro ao processar video', data: input.file }
+}
