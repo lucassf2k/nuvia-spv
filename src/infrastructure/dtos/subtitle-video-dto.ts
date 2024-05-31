8d52b2535d23e2c894f2a438eea9e2e39ad38662
lucas vinicius <lucassilved@gmail.com>
2024-05-31 20:00:58 -0300

    fix: error for the file property when it is empty or not sent

--- src/infrastructure/dtos/subtitle-video-dto.ts
+++ src/infrastructure/dtos/subtitle-video-dto.ts
@@ -4,7 +4,7 @@ export const subtitleVideoDTO = z.object({
 	file: z
 		.string({
 			invalid_type_error: 'Espera-se uma string',
-			required_error: '"file" é obrigatório',
+			required_error: 'Propriedade file é obrigatório',
 		})
-		.min(1, 'Não pode ser vázio'),
+		.min(1, 'Propriedade file não pode ser vázio'),
 })
